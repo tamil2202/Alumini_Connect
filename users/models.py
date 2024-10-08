466336b9f438ea1e01689ebf0d157459d6455141
tamil2202 <88816815+tamil2202@users.noreply.github.com>
2024-10-08 14:36:53 +0530

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 and img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
